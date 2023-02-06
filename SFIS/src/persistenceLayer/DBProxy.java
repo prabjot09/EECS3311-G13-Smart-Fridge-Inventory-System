a0884d4584e13c0b586efff7a9bde04223ea8775
Abdirahman Farah <abdiftw@gmail.com>
2023-02-06 13:37:58 -0500

    Cleaned up Database marginally

--- SFIS/src/persistenceLayer/DBProxy.java
+++ SFIS/src/persistenceLayer/DBProxy.java
@@ -9,11 +9,11 @@ public class DBProxy implements FridgeDatabase {
 	private static ArrayList<FridgeItem> fridgeDB = new ArrayList<FridgeItem>();
 	Fridge mainFridge = new Fridge(fridgeDB);
 	public DBProxy() {
-		mainFridge.addItem(new FridgeItem("Milk", 3));
-		mainFridge.addItem(new FridgeItem("Eggs", 12));
-		mainFridge.addItem(new FridgeItem("Cookies", 1));
-		mainFridge.addItem(new FridgeItem("Water", 54));
-		mainFridge.addItem(new FridgeItem("Juice", 4));
+		mainFridge.addItem(new FridgeItem("Milk - 3 Bags", 0));
+		mainFridge.addItem(new FridgeItem("Eggs - 12 Pack", 0));
+		
+		mainFridge.addItem(new FridgeItem("Water - Bottles", 0));
+		mainFridge.addItem(new FridgeItem("Juice - Carton", 0));
 	}
 	
 	public Fridge getDB() {
@@ -24,15 +24,7 @@ public class DBProxy implements FridgeDatabase {
 	@Override
 	public String findMatchingFoods(String name) {
 		
-		for (int x = 0; x < mainFridge.getFridgeSize(); x++) {
-			if (name.toLowerCase().compareTo(mainFridge.getFridge().get(x).getItem().toLowerCase()) == 0) {
-				return mainFridge.getFridge().get(x).getItem();
-				
-			}
-		
-		}
-		// TODO Auto-generated method stub
-		return "Could not find item";
+		return null;
 	}
 
 	@Override
