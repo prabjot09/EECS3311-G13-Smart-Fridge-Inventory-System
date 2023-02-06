0a5145e40470ebb8feb541bab6b166b1747f883c
Abdirahman Farah <abdiftw@gmail.com>
2023-02-06 13:51:52 -0500

    Refactored to store items in database as food items rather than fridge i

--- SFIS/src/persistenceLayer/DBProxy.java
+++ SFIS/src/persistenceLayer/DBProxy.java
@@ -2,38 +2,42 @@ package persistenceLayer;
 
 import java.util.ArrayList;
 
+import domainLayer.FoodItem;
 import domainLayer.Fridge;
-import domainLayer.FridgeItem;
+
 
 public class DBProxy implements FridgeDatabase {
-	private static ArrayList<FridgeItem> fridgeDB = new ArrayList<FridgeItem>();
+	//initialize DB by creating an arraylist of fridge items as well as our initial fridge.
+	private static ArrayList<FoodItem> fridgeDB = new ArrayList<FoodItem>();
 	Fridge mainFridge = new Fridge(fridgeDB);
+	
+	//Addition of our base items to the DB
 	public DBProxy() {
-		mainFridge.addItem(new FridgeItem("Milk - 3 Bags", 0));
-		mainFridge.addItem(new FridgeItem("Eggs - 12 Pack", 0));
-		
-		mainFridge.addItem(new FridgeItem("Water - Bottles", 0));
-		mainFridge.addItem(new FridgeItem("Juice - Carton", 0));
+		mainFridge.addItem(new FoodItem("Milk - 3 Bags"));
+		mainFridge.addItem(new FoodItem("Eggs - 12 Pack"));
+		mainFridge.addItem(new FoodItem("Water - Bottles"));
+		mainFridge.addItem(new FoodItem("Juice - Carton"));
 	}
 	
+	//Simple return method for our initial Fridge
 	public Fridge getDB() {
 		
 		return mainFridge;
 	}
 
-	@Override
+	
 	public String findMatchingFoods(String name) {
 		
 		return null;
 	}
 
-	@Override
+	
 	public void addItem() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	public void loadItems() {
 		// TODO Auto-generated method stub
 		
