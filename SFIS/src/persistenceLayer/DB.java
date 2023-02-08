7eb1d6408a5a99b3a096896a8ade0e80789bfbe9
dhaliwal-prabjot <prabs09@my.yorku.ca>
2023-02-07 22:25:48 -0500

    Fixed setFridge() method in FridgeDatabase and changed some formatting.

--- SFIS/src/persistenceLayer/DB.java
+++ SFIS/src/persistenceLayer/DB.java
@@ -3,15 +3,15 @@ package persistenceLayer;
 import java.util.ArrayList;
 import java.util.List;
 
+import domainLayer.Fridge;
 import domainLayer.StoredItem;
 
 public interface DB {
 	
-	
-	
-	
 	public ArrayList<String> findMatchingFoods(String name);
 	public void addItem(StoredItem item);
 	public List<StoredItem> loadItems();
+	public void updateFridge(Fridge fridge);
+	
 }
 
--- SFIS/src/persistenceLayer/ItemDatabase.java
+++ SFIS/src/persistenceLayer/ItemDatabase.java
@@ -6,11 +6,12 @@ public class ItemDatabase {
 	private ArrayList<String> ItemDB = new ArrayList<String>();
 
 	public ItemDatabase() {
-	ItemDB.add("Milk - 3 Bags");
-	ItemDB.add("Juice - Carton");
-	ItemDB.add("Eggs - Dozen");
-	ItemDB.add("Butter - Sticks");
+		ItemDB.add("Milk - 3 Bags");
+		ItemDB.add("Juice - Carton");
+		ItemDB.add("Eggs - Dozen");
+		ItemDB.add("Butter - Sticks");
 	}
+	
 	public ArrayList<String> getDB() {
 		return ItemDB;
 	}
@@ -27,8 +28,7 @@ public class ItemDatabase {
 		if (holdMatch.size() != 0) {
 			return holdMatch;
 		}
-		else
-		{
+		else {
 			return null;
 		}
 	}
