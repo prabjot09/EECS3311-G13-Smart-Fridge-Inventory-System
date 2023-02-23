e477cffc69f9657a286a0731bb3a0cff05a0a9ce
dhaliwal-prabjot <prabs09@my.yorku.ca>
2023-02-23 01:07:11 -0500

    Can specify type of food type (creator and stock type)

--- SFIS/src/domainLayer/FoodItem.java
+++ SFIS/src/domainLayer/FoodItem.java
@@ -1,7 +1,33 @@
 package domainLayer;
 
 public class FoodItem {
+	public enum StockType {
+		CONTINUOUS,
+		DISCRETE
+	}
+	
+	public enum CreationType {
+		USER,
+		PRESET
+	}
+	
 	private String name;
+	private StockType stockType;
+	private CreationType creator;
+	
+	public FoodItem() {
+		
+	}
+	
+	public FoodItem(String name, StockType stockType) {
+		this.name = name;
+		this.stockType = stockType;
+	}
+	
+	public FoodItem(String name, StockType stockType, CreationType creator) {
+		this(name, stockType);
+		this.creator = creator;
+	}
 	
 	public String getName() {
 		return this.name;
@@ -9,5 +35,21 @@ public class FoodItem {
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public StockType getStockType() {
+		return this.stockType;
+	}
+	
+	public void setStockType(StockType type) {
+		this.stockType = type;
+	}
+	
+	public CreationType getCreator() {
+		return this.creator;
+	}
+	
+	public void setCreator(CreationType creator) {
+		this.creator = creator;
 	}
 }
