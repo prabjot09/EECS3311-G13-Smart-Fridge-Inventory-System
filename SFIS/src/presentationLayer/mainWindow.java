d4d6d022b25a27d7be65b24eb608f65c5c117fb8
StraightUpRice <eric905@yorku.ca>
2023-03-09 04:38:00 -0500

    none breaking initial grocery view commit

--- SFIS/src/presentationLayer/mainWindow.java
+++ SFIS/src/presentationLayer/mainWindow.java
@@ -128,11 +128,22 @@ public class mainWindow implements ActionListener{
 	    viewManager = new ListViewManager(views);
 	    viewPanel = new JPanel();
 	    viewPanel.setBackground(Color.black);
-	    viewPanel.setPreferredSize(new Dimension(820, 400));
+	    viewPanel.setPreferredSize(new Dimension(400, 400));
 	    viewPanel.add((JPanel) viewManager.getCurrentView());
 	    
 	    searchPanel.add(viewPanel);
 	    // END: Search and Item List Panel
+	    
+	    // Grocery List Panel
+	    JPanel rightPanel = new JPanel();
+	    rightPanel.setBackground(Color.black);
+	    rightPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	    rightPanel.setPreferredSize(new Dimension(400, 400));
+	    
+	    
+	    rightPanel.add(new GroceryListView());
+	    jframe.add(rightPanel, BorderLayout.EAST);
+
 	    
 	    // Update DB when closing the window
 	    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -145,7 +156,7 @@ public class mainWindow implements ActionListener{
 	    
 	    // set the jframe size and location, and make it visible
 	    jframe.getContentPane().setBackground(Color.black);
-	    jframe.setPreferredSize(new Dimension(1000, 650));
+	    jframe.setPreferredSize(new Dimension(1260, 650));
 	    jframe.pack();
 	    jframe.setLocationRelativeTo(null);
 	    jframe.setVisible(true);
